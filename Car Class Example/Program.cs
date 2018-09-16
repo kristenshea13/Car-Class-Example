967706a628d708a603b6e90e6a25ceb98a77d8a3
Kristen Shea <kstar61@live.com>
2018-09-16 14:47:45 -0500

    Add project files.

---
+++ Car Class Example/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Car_Class_Example
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
